efa009334de4f9258a7215241ba71d206a453ea0
oodegard <oodegard@gmail.com>
2025-07-01 09:16:39 +0200

    Added export physical dimensions, and lut

--- bioformats2tif.py
+++ bioformats2tif.py
@@ -1,64 +1,86 @@
-#@ File   (label = "Input directory", style = "directory") srcFile
-#@ File   (label = "Output directory", style = "directory") dstFile
+#@ File	 (label = "Input directory", style = "directory") srcFile
+#@ File	 (label = "Output directory", style = "directory") dstFile
 #@ String (label = "File extension", value=".tif") ext
 #@ String (label = "File name contains", value = "") containString
 #@ boolean(label = "Keep directory structure when saving", value = true) keepDirectories
 #@ String (label = "Save format", choices={"Standard ImageJ TIF", "OME-TIF"}, style="listBox") saveType
 
 import os
-from ij import IJ, ImagePlus, ImageStack
+from ij import IJ, ImagePlus, ImageStack, CompositeImage
 from loci.plugins.in import ImporterOptions
 from loci.plugins import BF
 
+
 def run():
-    srcDir = srcFile.getAbsolutePath()
-    dstDir = dstFile.getAbsolutePath()
-    for root, directories, filenames in os.walk(srcDir):
-        filenames.sort()
-        for filename in filenames:
-            if not filename.endswith(ext):
-                continue
-            if containString not in filename:
-                continue
-            process(srcDir, dstDir, root, filename, keepDirectories)
+	srcDir = srcFile.getAbsolutePath()
+	dstDir = dstFile.getAbsolutePath()
+	for root, directories, filenames in os.walk(srcDir):
+		filenames.sort()
+		for filename in filenames:
+			if not filename.endswith(ext):
+				continue
+			if containString not in filename:
+				continue
+			process(srcDir, dstDir, root, filename, keepDirectories)
 
 def process(srcDir, dstDir, currentDir, fileName, keepDirectories):
-    print("Processing:", fileName)
-    options = ImporterOptions()
-    options.setColorMode(ImporterOptions.COLOR_MODE_GRAYSCALE)
-    options.setOpenAllSeries(True)
-    options.setVirtual(True)
-    options.setId(os.path.join(currentDir, fileName))
-    
-    imps = BF.openImagePlus(options)
-    for i, imp in enumerate(imps):
-        outname, _ = os.path.splitext(fileName)
-        outname = outname + "_" + str(i+1)
-        
-        saveDir = currentDir.replace(srcDir, dstDir) if keepDirectories else dstDir
-        if not os.path.exists(saveDir):
-            os.makedirs(saveDir)
-        
-        output_path = os.path.join(saveDir, outname + ".ome.tif") if saveType == "OME-TIF" else os.path.join(saveDir, outname + ".tif")
-        print("Saving to:", output_path)
+	print("Processing:", fileName)
+	options = ImporterOptions()
+	options.setColorMode(ImporterOptions.COLOR_MODE_GRAYSCALE)
+	options.setOpenAllSeries(True)
+	options.setVirtual(True)
+	options.setId(os.path.join(currentDir, fileName))
+	
+	imps = BF.openImagePlus(options)
+	for i, imp in enumerate(imps):
+		outname, _ = os.path.splitext(fileName)
+		outname = outname + "_" + str(i+1)
+		
+		saveDir = currentDir.replace(srcDir, dstDir) if keepDirectories else dstDir
+		if not os.path.exists(saveDir):
+				os.makedirs(saveDir)
+		
+		output_path = os.path.join(saveDir, outname + ".ome.tif") if saveType == "OME-TIF" else os.path.join(saveDir, outname + ".tif")
+		print("Saving to:", output_path)
 
-        # Duplicate pixel data to a clean ImagePlus (strips metadata)
-        stack = ImageStack(imp.getWidth(), imp.getHeight())
-        for s in range(1, imp.getStackSize() + 1):
-            stack.addSlice(imp.getStack().getProcessor(s))
-        blank_imp = ImagePlus(imp.getTitle(), stack)
-        blank_imp.setDimensions(imp.getNChannels(), imp.getNSlices(), imp.getNFrames())
 
-        # Save
-        try:
-            if saveType == "Standard ImageJ TIF":
-                IJ.saveAs(blank_imp, "Tiff", output_path)
-            elif saveType == "OME-TIF":
-                IJ.run(blank_imp, "Bio-Formats Exporter", "save=[" + output_path + "] compression=Uncompressed")
-        except Exception as e:
-            print("Failed to save:", e)
-        
-        blank_imp.close()
-        imp.close()
+		# Duplicate pixel data to a clean ImagePlus (strips metadata)
+	
+		stack = ImageStack(imp.getWidth(), imp.getHeight())
+	
+		for s in range(1, imp.getStackSize() + 1):
+			stack.addSlice(imp.getStack().getProcessor(s))
+		blank_imp = ImagePlus(imp.getTitle(), stack)
+		blank_imp.setDimensions(imp.getNChannels(), imp.getNSlices(), imp.getNFrames())
+		blank_imp.setOpenAsHyperStack(True)
+	
+		# Copy calibration
+		blank_imp.setCalibration(imp.getCalibration())
+	
+		# Copy LUTs if present
+		
+		
+		if imp.isComposite():
+		    luts = imp.getLuts()
+		    if luts is not None:
+		        # Convert blank_imp to CompositeImage so we can set per-channel LUTs
+		        comp_imp = CompositeImage(blank_imp, imp.getMode())  # Use same composite mode (e.g. COMPOSITE, COLOR, etc.)
+		        for c in range(len(luts)):
+		            comp_imp.setChannelLut(luts[c], c+1)  # channel indices are 1-based in ImageJ
+		        comp_imp.setDisplayMode(imp.getDisplayMode())
+		        blank_imp = comp_imp  # Overwrite the reference so we save the composite version
+			
+		 
+		# Save
+		try:
+			if saveType == "Standard ImageJ TIF":
+				IJ.saveAs(blank_imp, "Tiff", output_path)
+			elif saveType == "OME-TIF":
+				IJ.run(blank_imp, "Bio-Formats Exporter", "save=[" + output_path + "] compression=Uncompressed")
+		except Exception as e:
+			print("Failed to save:", e)
+			
+		blank_imp.close()
+		imp.close()
 
 run()
